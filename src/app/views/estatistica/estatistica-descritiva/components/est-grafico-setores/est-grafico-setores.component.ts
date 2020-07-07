239697d09e3208efbc0ac08a919ee9650973a453
Unknown <adrielgoulartdeabranjo@gmail.com>
2020-07-07 19:30:29 -0300

    testando deploy com CanvasJs

--- src/app/views/estatistica/estatistica-descritiva/components/est-grafico-setores/est-grafico-setores.component.ts
+++ src/app/views/estatistica/estatistica-descritiva/components/est-grafico-setores/est-grafico-setores.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Chart } from 'chart.js'
 
+declare var CanvasJS: any;
 
 @Component({
   selector: 'ifmath-est-grafico-setores',
@@ -8,6 +9,9 @@ import { Chart } from 'chart.js'
   styleUrls: ['./est-grafico-setores.component.scss']
 })
 export class EstGraficoSetoresComponent implements OnInit {
+
+  canvasObj : any;
+  hash:String;
 
   public pieChartLabels = ['Sales Q1', 'Sales Q2', 'Sales Q3', 'Sales Q4'];
   public pieChartData = [120, 150, 180, 90];
@@ -17,6 +21,36 @@ export class EstGraficoSetoresComponent implements OnInit {
 
   ngOnInit() {
 
+    var chart = new CanvasJS.Chart("chartContainer", {
+      theme: "light2", // "light1", "light2", "dark1", "dark2"
+      exportEnabled: true,
+      animationEnabled: true,
+      title: {
+        text: "Desktop Browser Market Share in 2016"
+      },
+      data: [{
+        type: "pie",
+        startAngle: 25,
+        toolTipContent: "<b>{label}</b>: {y}%",
+        showInLegend: "true",
+        legendText: "{label}",
+        indexLabelFontSize: 16,
+        indexLabel: "{label} - {y}%",
+        dataPoints: [
+          { y: 51.08, label: "Chrome" },
+          { y: 27.34, label: "Internet Explorer" },
+          { y: 10.62, label: "Firefox" },
+          { y: 5.02, label: "Microsoft Edge" },
+          { y: 4.07, label: "Safari" },
+          { y: 1.22, label: "Opera" },
+          { y: 0.44, label: "Others" }
+        ]
+      }]
+    });
+    chart.render();
+    
+
+    /*
     var myChart = new Chart("estChart", {
       type: 'pie',
       data: {
@@ -31,8 +65,11 @@ export class EstGraficoSetoresComponent implements OnInit {
           ]
         }],
       },
+      options: {
+          animation: {animateScale: true},
+        }, 
       
-  });
+  });*/
 
   }
 
